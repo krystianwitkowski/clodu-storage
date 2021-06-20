089a64d85f95a63a0ca691c7b8db7e8e7d27c6e4
krystianwitkowski <krystian.witkowski090@gmail.com>
2021-06-20 09:37:54 -0700

    created menu component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,36 @@
 <template>
   <v-app>
-    <v-main >
-    
+    <v-main class="d-flex flex-row">
+        <v-row>
+          <v-col cols="3">
+            <Menu />
+          </v-col>
+          <v-col cols="9">
+            <Board />
+          </v-col>
+        </v-row>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import Menu from './components/Menu.vue';
+import Board from './components/Board.vue';
+
 export default {
   name: 'App',
+  components: {
+    Menu,
+    Board
+  }
 };
 </script>
+
+<style>
+
+  .v-main__wrap {
+    display: flex;
+    flex-direction: row;
+  }
+
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-card
+      class="mx-auto pr-4"
+      width="100%"
+      height="100vh"
+      tile
+    >
+    <v-btn
+      rounded
+      width="120"
+      height="45"
+      class="mt-8 mb-8 ml-7"
+      style="color: #888888;"
+    >
+      <v-icon>
+        mdi-plus
+      </v-icon>
+    </v-btn>
+
+      <v-list dense shaped class="pa-0">
+        <v-list-item-group
+          v-model="selectedItem"
+          color="primary"
+        >
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+          >
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+      <v-divider class="mt-1 mb-1"></v-divider>
+      <v-list dense>
+        <v-list-item-group>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-cloud-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Data storage</v-list-item-title>
+              <v-list-item-title class="mt-2"><v-progress-linear value="15"></v-progress-linear></v-list-item-title>
+              <v-list-item-title class="mt-1">used 1MB of 2GB</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  data(){
+    return {
+      selectedItem: 1,
+      items: [
+        { text: 'My drive', icon: 'mdi-file-document-multiple-outline' },
+        { text: 'Latest', icon: 'mdi-clock-time-eight-outline' },
+        { text: 'Trash', icon: 'mdi-trash-can-outline' },
+      ],
+    }
+  }
+};
+</script>
