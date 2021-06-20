fc73146731e2948062fdf3848755a0cd7b9d615f
krystianwitkowski <krystian.witkowski090@gmail.com>
2021-06-20 14:48:38 -0700

    created MyDrive component

---
+++ src/components/Board.vue
@@ -0,0 +1,19 @@
+<template>
+    <MyDrive />
+</template>
+
+<script>
+import MyDrive from './MyDrive.vue'
+
+export default {
+    name: 'Board',
+    components: {
+        MyDrive
+    }
+}
+</script>
+
+<style>
+
+
+</style>
---
+++ src/components/MyDrive.vue
@@ -0,0 +1,336 @@
+<template>
+  <v-container>
+    <v-container class="pa-0">
+      <v-row>
+        <v-col>
+          <p style="color: #585858" class="subtitle-1 pl-3 font-weight-medium">
+            My drive
+          </p>
+          <v-divider></v-divider>
+        </v-col>
+      </v-row>
+      <v-row class="mt-8">
+        <v-col class="pa-0">
+          <p style="color: #585858" class="subtitle-2 pl-3">Folders</p>
+        </v-col>
+      </v-row>
+      <v-row class="mt-0">
+        <v-col style="flex-basis: 33%">
+          <v-list-item-group>
+            <v-list-item
+              class="d-flex"
+              style="
+                border: 1px solid rgba(0, 0, 0, 0.12);
+                height: 50px;
+                border-radius: 8px;
+              "
+            >
+              <v-list-item-icon class="ma-0" style="align-self: center">
+                <v-icon color="#A9A9A9" style="font-size: 22px"
+                  >mdi-folder</v-icon
+                >
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title
+                  style="color: #585858"
+                  class="subtitle-2 ml-3"
+                  >folder1</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-col>
+        <v-col style="flex-basis: 33%">
+          <v-list-item-group>
+            <v-list-item
+              class="d-flex"
+              style="
+                border: 1px solid rgba(0, 0, 0, 0.12);
+                height: 50px;
+                border-radius: 8px;
+              "
+            >
+              <v-list-item-icon class="ma-0" style="align-self: center">
+                <v-icon color="#A9A9A9" style="font-size: 22px"
+                  >mdi-folder</v-icon
+                >
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title
+                  style="color: #585858"
+                  class="subtitle-2 ml-3"
+                  >folder1</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-col>
+        <v-col style="flex-basis: 33%">
+          <v-list-item-group>
+            <v-list-item
+              class="d-flex"
+              style="
+                border: 1px solid rgba(0, 0, 0, 0.12);
+                height: 50px;
+                border-radius: 8px;
+              "
+            >
+              <v-list-item-icon class="ma-0" style="align-self: center">
+                <v-icon color="#A9A9A9" style="font-size: 22px"
+                  >mdi-folder</v-icon
+                >
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title
+                  style="color: #585858"
+                  class="subtitle-2 ml-3"
+                  >folder1</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-col>
+        <v-col style="flex-basis: 33%">
+          <v-list-item-group>
+            <v-list-item
+              class="d-flex"
+              style="
+                border: 1px solid rgba(0, 0, 0, 0.12);
+                height: 50px;
+                border-radius: 8px;
+              "
+            >
+              <v-list-item-icon class="ma-0" style="align-self: center">
+                <v-icon color="#A9A9A9" style="font-size: 22px"
+                  >mdi-folder</v-icon
+                >
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title
+                  style="color: #585858"
+                  class="subtitle-2 ml-3"
+                  >folder1</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-col>
+        <v-col style="flex-basis: 33%">
+          <v-list-item-group>
+            <v-list-item
+              class="d-flex"
+              style="
+                border: 1px solid rgba(0, 0, 0, 0.12);
+                height: 50px;
+                border-radius: 8px;
+              "
+            >
+              <v-list-item-icon class="ma-0" style="align-self: center">
+                <v-icon color="#A9A9A9" style="font-size: 22px"
+                  >mdi-folder</v-icon
+                >
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title
+                  style="color: #585858"
+                  class="subtitle-2 ml-3"
+                  >folder1</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-col>
+        <v-col style="flex-basis: 33%">
+          <v-list-item-group>
+            <v-list-item
+              class="d-flex"
+              style="
+                border: 1px solid rgba(0, 0, 0, 0.12);
+                height: 50px;
+                border-radius: 8px;
+              "
+            >
+              <v-list-item-icon class="ma-0" style="align-self: center">
+                <v-icon color="#A9A9A9" style="font-size: 22px"
+                  >mdi-folder</v-icon
+                >
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title
+                  style="color: #585858"
+                  class="subtitle-2 ml-3"
+                  >folder1</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container class="pa-0 mt-8">
+      <v-row>
+        <v-col class="pa-0">
+          <p style="color: #585858" class="subtitle-2 pl-3">Files</p>
+        </v-col>
+      </v-row>
+      <v-row class="mt-0">
+        <v-col style="flex-basis: 33%">
+          <v-list-item-group>
+            <v-list-item
+              class="flex-column"
+              style="
+                padding: 30px 0;
+                min-height: initial;
+                border: 1px solid rgba(0, 0, 0, 0.12);
+                height: 170px;
+                border-radius: 8px;
+              "
+            >
+              <v-list-item-icon class="ma-0" style="align-self: center">
+                <v-icon color="#A9A9A9" style="font-size: 52px"
+                  >mdi-file</v-icon
+                >
+              </v-list-item-icon>
+              <v-list-item-content style="flex: initial; padding-top: 25px">
+                <v-list-item-title style="color: #585858" class="subtitle-2"
+                  >landing page.rar</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-col>
+        <v-col style="flex-basis: 33%">
+          <v-list-item-group>
+            <v-list-item
+              class="flex-column"
+              style="
+                padding: 30px 0;
+                min-height: initial;
+                border: 1px solid rgba(0, 0, 0, 0.12);
+                height: 170px;
+                border-radius: 8px;
+              "
+            >
+              <v-list-item-icon class="ma-0" style="align-self: center">
+                <v-icon color="#A9A9A9" style="font-size: 52px"
+                  >mdi-file</v-icon
+                >
+              </v-list-item-icon>
+              <v-list-item-content style="flex: initial; padding-top: 25px">
+                <v-list-item-title style="color: #585858" class="subtitle-2"
+                  >landing page.rar</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-col>
+        <v-col style="flex-basis: 33%">
+          <v-list-item-group>
+            <v-list-item
+              class="flex-column"
+              style="
+                padding: 30px 0;
+                min-height: initial;
+                border: 1px solid rgba(0, 0, 0, 0.12);
+                height: 170px;
+                border-radius: 8px;
+              "
+            >
+              <v-list-item-icon class="ma-0" style="align-self: center">
+                <v-icon color="#A9A9A9" style="font-size: 52px"
+                  >mdi-file</v-icon
+                >
+              </v-list-item-icon>
+              <v-list-item-content style="flex: initial; padding-top: 25px">
+                <v-list-item-title style="color: #585858" class="subtitle-2"
+                  >landing page.rar</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-col>
+        <v-col style="flex-basis: 33%">
+          <v-list-item-group>
+            <v-list-item
+              class="flex-column"
+              style="
+                padding: 30px 0;
+                min-height: initial;
+                border: 1px solid rgba(0, 0, 0, 0.12);
+                height: 170px;
+                border-radius: 8px;
+              "
+            >
+              <v-list-item-icon class="ma-0" style="align-self: center">
+                <v-icon color="#A9A9A9" style="font-size: 52px"
+                  >mdi-file</v-icon
+                >
+              </v-list-item-icon>
+              <v-list-item-content style="flex: initial; padding-top: 25px">
+                <v-list-item-title style="color: #585858" class="subtitle-2"
+                  >landing page.rar</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-col>
+        <v-col style="flex-basis: 33%">
+          <v-list-item-group>
+            <v-list-item
+              class="flex-column"
+              style="
+                padding: 30px 0;
+                min-height: initial;
+                border: 1px solid rgba(0, 0, 0, 0.12);
+                height: 170px;
+                border-radius: 8px;
+              "
+            >
+              <v-list-item-icon class="ma-0" style="align-self: center">
+                <v-icon color="#A9A9A9" style="font-size: 52px"
+                  >mdi-file</v-icon
+                >
+              </v-list-item-icon>
+              <v-list-item-content style="flex: initial; padding-top: 25px">
+                <v-list-item-title style="color: #585858" class="subtitle-2"
+                  >landing page.rar</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-col>
+        <v-col style="flex-basis: 33%">
+          <v-list-item-group>
+            <v-list-item
+              class="flex-column"
+              style="
+                padding: 30px 0;
+                min-height: initial;
+                border: 1px solid rgba(0, 0, 0, 0.12);
+                height: 170px;
+                border-radius: 8px;
+              "
+            >
+              <v-list-item-icon class="ma-0" style="align-self: center">
+                <v-icon color="#A9A9A9" style="font-size: 52px"
+                  >mdi-file</v-icon
+                >
+              </v-list-item-icon>
+              <v-list-item-content style="flex: initial; padding-top: 25px">
+                <v-list-item-title style="color: #585858" class="subtitle-2"
+                  >landing page.rar</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "MyDrive",
+};
+</script>
