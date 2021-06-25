f596b8f37580439e45f5d23a0461501aeb3982d5
krystianwitkowski <krystian.witkowski090@gmail.com>
2021-06-25 12:04:46 -0700

    rewrited api to mongodb

--- server/routes/uploadFile.js
+++ server/routes/uploadFile.js
@@ -2,62 +2,83 @@ const express = require("express");
 const Router = express.Router();
 const mimeTypes = require('../utilities/mime-types.js');
 
+const { MongoClient } = require('mongodb');
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.ex8xh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connect = client.connect()
+
 Router.route("/")
 .post(async (req, res) => {
-    /*
-    const users = await db.collection('users').doc("0").get();
-    
-    const update = await db.collection('users').doc("0").update({
-        files: [...users.data().files, {
-            id: users.data().files.length > 0 ? users.data().files.length : 0,
-            file: JSON.stringify(req.files.file),
-            base64: Buffer.from(req.files.file.data).toString('base64'),
-            icons: mimeTypes.find(type => type.mimetype === req.files.file.mimetype),
-            trash: false,
-            starred: false
-        }]
-    });
 
-    res.json(update)
-    */
+
+    const db = client.db("cloud-drive-db");
+    const users = await db.collection('users');
+
+    const currentDoc = await users.findOne({ id: 0 })
+
+    const update = await users.updateOne({ id: 0 }, {
+        $set: {
+            files: [...currentDoc.files, {
+                id: currentDoc.files.length > 0 ? currentDoc.files.length : 0,
+                file: JSON.stringify(req.files.file),
+                base64: Buffer.from(req.files.file.data).toString('base64'),
+                icons: mimeTypes.find(type => type.mimetype === req.files.file.mimetype),
+                trash: false,
+                starred: false 
+            }]
+        }
+    })
+    
+    res.json(update);
 })
 .get(async(req, res) => {
-    /*
-    const users = await db.collection('users').doc("0").get();
     
+    const db = client.db("cloud-drive-db");
+    const users = await db.collection('users');
+
+    const currentDoc = await users.findOne({ id: 0 })
+
     if(req.query.file){
-        const last = users.data().files.find((file, i, arr) => (arr.length - 1 ) === i)
+        const last = currentDoc.files.find((file, i, arr) => (arr.length - 1 ) === i)
         res.json([last])
     }
 
     else {
-        res.json(users.data().files)
+        res.json(currentDoc.files)
     }
-    */
+    
 })
 .put(async (req, res) => {
-    /*
-    const users = await db.collection('users').doc("0").get();
+
+    const db = client.db("cloud-drive-db");
+    const users = await db.collection('users');
+
+    const currentDoc = await users.findOne({ id: 0 })
 
     if(req.query.trash){
-        const modify = users.data().files.map(file => file.id === JSON.parse(req.query.id) ? { ...file, trash: JSON.parse(req.query.trash) } : { ...file })
-    
-        const update = await db.collection('users').doc("0").update({
-            files: [...modify]
-        });
-    
+        const modify = currentDoc.files.map(file => file.id === JSON.parse(req.query.id) ? { ...file, trash: JSON.parse(req.query.trash) } : { ...file })
+        
+        const update = await users.updateOne({ id: 0 }, {
+            $set: {
+                files: [...modify]
+            }
+        })
+
         res.json(update)
     }
     else if(req.query.starred){
-        const modify = users.data().files.map(file => file.id === JSON.parse(req.query.id) ? { ...file, starred: JSON.parse(req.query.starred) } : { ...file })
+        const modify = currentDoc.files.map(file => file.id === JSON.parse(req.query.id) ? { ...file, starred: JSON.parse(req.query.starred) } : { ...file })
     
-        const update = await db.collection('users').doc("0").update({
-            files: [...modify]
-        });
-    
+        const update = await users.updateOne({ id: 0 }, {
+            $set: {
+                files: [...modify]
+            }
+        })
+
         res.json(update)
     }
-    */
 })
 
 module.exports = Router;
